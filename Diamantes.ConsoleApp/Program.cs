167f86e9cf0bbaedf047c8db98ff5824ec91e917
nedzie <marcos-elivelton@hotmail.com>
2022-02-21 16:45:12 -0300

    Estruturação

--- Diamantes.ConsoleApp/Program.cs
+++ Diamantes.ConsoleApp/Program.cs
@@ -6,20 +6,26 @@ namespace Diamantes.ConsoleApp
     {
         static void Main(string[] args)
         {
-            int tamanho, espacosEmBranco ;
+            int tamanho, espacosEmBranco, numeroDeGiros, quantidadeDeX;
+            RecebimentoDeInformacoes(out tamanho, out espacosEmBranco, out numeroDeGiros, out quantidadeDeX);
+            ImprimeParteSuperior(ref espacosEmBranco, numeroDeGiros, ref quantidadeDeX);
+            ImprimeParteInferior(tamanho, ref espacosEmBranco, numeroDeGiros, ref quantidadeDeX);
+        }
+        #region Métodos
+        static void RecebimentoDeInformacoes(out int tamanho, out int espacosEmBranco, out int numeroDeGiros, out int quantidadeDeX)
+        {
             Console.WriteLine("Digite um número ímpar: ");
             tamanho = Convert.ToInt32(Console.ReadLine());
             espacosEmBranco = (tamanho - 1) / 2;
-            int numeroDeGiros = (tamanho - 1) / 2;
-            int quantidadeDeX = 1;
-
-            for(int i = 0; i < numeroDeGiros; i++) // Controla espaços e x
+            numeroDeGiros = (tamanho - 1) / 2;
+            quantidadeDeX = 1;
+        }
+        private static void ImprimeParteSuperior(ref int espacosEmBranco, int numeroDeGiros, ref int quantidadeDeX)
+        {
+            for (int i = 0; i < numeroDeGiros; i++) // Controla espaços e x
             {
-                for(int j = 0; j < espacosEmBranco; j++)
-                {
-                    Console.Write(" ");
-                }
-                for(int j = 0; j < quantidadeDeX; j++)
+                NewMethod(espacosEmBranco);
+                for (int j = 0; j < quantidadeDeX; j++)
                 {
                     Console.Write("x");
                 }
@@ -27,7 +33,18 @@ namespace Diamantes.ConsoleApp
                 quantidadeDeX += 2;
                 Console.WriteLine();
             }
+        }
 
+        private static void NewMethod(int espacosEmBranco)
+        {
+            for (int j = 0; j < espacosEmBranco; j++)
+            {
+                Console.Write(" ");
+            }
+        }
+
+        private static void ImprimeParteInferior(int tamanho, ref int espacosEmBranco, int numeroDeGiros, ref int quantidadeDeX)
+        {
             for (int i = 0; i < tamanho; i++)
             {
                 Console.Write("x");
@@ -51,5 +68,6 @@ namespace Diamantes.ConsoleApp
                 Console.WriteLine();
             }
         }
+        #endregion
     }
 }
